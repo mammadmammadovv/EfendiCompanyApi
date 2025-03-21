5d7b2c8641eace19950dc20ac28f801bcf193702
Rufat Ismayilov <rufet.ismayilov.1999@gmail.com>
2025-03-21 21:58:46 +0400

    added-datas-to-db

--- EfendiCompany/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ EfendiCompany/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298847b82c60f4990d28edb3e4337cb5e6fb8bc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06b6e5f22bd1930ac806860b89a71766bda1a5e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
