8dfffa96634e8d19cefc0af5ce463a36132109ed
Rufat Ismayilov <rufet.ismayilov.1999@gmail.com>
2025-03-29 16:15:02 +0400

    added-logo

--- EfendiCompany/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ EfendiCompany/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f43cc0e4769e62682e814618eea6ead5bea910fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d560ab0db70057693a5e224b4bbd64b40c89141d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
