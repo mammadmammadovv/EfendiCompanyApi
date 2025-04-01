4d59ff5fc067c9def5fc2e61ded5ceff943b746c
mammadovmammad <mammadmammadovv@gmail.com>
2025-04-01 21:16:05 +0400

    change logo

--- EfendiCompany/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ EfendiCompany/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35bcbdb9567f216de533382226c27aced87dc7e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5faa2c94ea3f8eb001a90e3e61bdf03e9a23e0fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
