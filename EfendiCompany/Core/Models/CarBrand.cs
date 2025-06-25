bec2618701e9079aeef0ed09b21399da63a3a113
Mammad Mammadov <mammad.mammadov@ateshgah.com>
2025-06-25 16:56:41 +0400

    add carbrand and car model

---
+++ EfendiCompany/Core/Models/CarBrand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CarBrand
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Country { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
---
+++ EfendiCompany/Core/Models/CarModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CarModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; };
+    public int BrandId { get; set; }
+    public int? StartYear { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
